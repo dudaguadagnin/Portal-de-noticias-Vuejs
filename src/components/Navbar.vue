534fbe1b1dc5d564386976808a1bfd27925b358e
Eduarda Guadagnin <eduarda.guadagnin@CTNDELLCMZVJB3.pampa.compasso>
2021-03-22 11:20:35 -0300

    organizando barra de navegação

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,47 +1,26 @@
 <template>
-<div>
- 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#" @click="$emit('home')"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Portal de Notícias</font></font></a>
-  <button @click="showNav()"  class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="true" aria-label="Alternar de navegação">
+
+
+
+<nav class="navbar navbar-expand-lg navbar-light nav-cor">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
+  <a class="navbar-brand" href="#" @click="$emit('home')">Portal de Notícias</a>
 
-  <div class="navbar-collapse collapse show" id="navbarTogglerDemo02" style="">
+  <div class="collapse navbar-collapse cor" id="navbarTogglerDemo03">
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
       <li class="nav-item active">
-        <a class="nav-link" href="#" @click="$emit('science')">
-            <font style="vertical-align: inherit;">
-                <font style="vertical-align: inherit;">
-                    Science
-                </font>
-            </font>
-            <span class="sr-only">
-                <font style="vertical-align: inherit;">
-                    <font style="vertical-align: inherit;">
-                        (atual)
-                    </font>
-                </font>
-            </span>
-        </a>
+        <a class="nav-link" href="#" @click="$emit('science')">Science <span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="#" @click="$emit('technology')">
-            <font style="vertical-align: inherit;">
-                <font style="vertical-align: inherit;">
-                    Tecnology
-                </font>
-            </font>
-        </a>
+    <li class="nav-item active">
+        <a class="nav-link" href="#" @click="$emit('technology')">Technology <span class="sr-only">(current)</span></a>
       </li>
+
     </ul>
-    
   </div>
 </nav>
 
-
-
-</div>
 
 </template>
 
@@ -50,19 +29,10 @@
 <script>
 
 export default {
-  data(){
-    return{
-      Navshow : false,
-
-    }
-  },
-  methods:{
-    showNav(post){
-      this.Navshow = true
-    }
-  },
-
-
-
 };
 </script>
+<style scoped>
+.nav-cor{
+    background-color:rgb(81, 163, 163);
+}
+</style>
